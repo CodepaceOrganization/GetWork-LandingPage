e8214c578f8d197343b69075ce69bd405594a142
Adriano Sebastian Cruz Palomino <u202210697@upc.edu.pe>
2024-04-07 23:24:48 -0500

    fix: Fix responsive layout on mobile devices and navbar functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("down",window.scrollY>0);
 })
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const hamburgerBtn = document.querySelector('.hamburger-btn');
     const navbarList = document.querySelector('.navbar-list');
@@ -11,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     const navbarLinks = navbarList.querySelectorAll('a');
+    
     navbarLinks.forEach(function(link) {
         link.addEventListener('click', function() {
             navbarList.classList.remove('active');
